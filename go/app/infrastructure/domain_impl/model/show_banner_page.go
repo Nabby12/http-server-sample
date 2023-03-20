b34be460ee1ee93b4bfe0d9f3f3e29fa45404cff
Nabby12 <Nabby12@users.noreply.github.com>
2023-03-20 21:47:30 +0900

    add/infrastructure-layer

---
+++ go/app/infrastructure/domain_impl/model/show_banner_page.go
@@ -0,0 +1,58 @@
+package model
+
+import "go-http-server/internal/domain/model"
+
+type showBannerPage struct {
+	title       string
+	header      string
+	currentTime string
+	startTime   string
+	endTime     string
+	bannerFlag  bool
+}
+
+func NewShowBannerPage(
+	title string,
+	header string,
+	currentTime string,
+	startTime string,
+	endTime string,
+	bannerFlag bool,
+) model.ShowBannerPage {
+	return &showBannerPage{
+		title:       title,
+		header:      header,
+		currentTime: currentTime,
+		startTime:   startTime,
+		endTime:     endTime,
+		bannerFlag:  bannerFlag,
+	}
+}
+
+func (sbp *showBannerPage) Title() string {
+	return sbp.title
+}
+
+func (sbp *showBannerPage) Header() string {
+	return sbp.header
+}
+
+func (sbp *showBannerPage) CurrentTime() string {
+	return sbp.currentTime
+}
+
+func (sbp *showBannerPage) StartTime() string {
+	return sbp.startTime
+}
+
+func (sbp *showBannerPage) EndTime() string {
+	return sbp.endTime
+}
+
+func (sbp *showBannerPage) BannerFlag() bool {
+	return sbp.bannerFlag
+}
+
+func (sbp *showBannerPage) UpdateBannerFlag(flag bool) {
+	sbp.bannerFlag = flag
+}
